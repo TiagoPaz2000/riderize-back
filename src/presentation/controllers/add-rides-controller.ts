7bb62d43e2271f7fdaa8f4e61adb1873108e7f5e
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-27 19:02:53 -0300

    add: implement ride validator on controller

--- src/presentation/controllers/add-rides-controller.ts
+++ src/presentation/controllers/add-rides-controller.ts
@@ -1,10 +1,18 @@
+import RideValidator from '@/domain/usecases/ride-validator'
 import { IController } from '../protocols/controller-protocol'
 import { IHttpRequest, IHttpResponse } from '../protocols/http-protocol'
 
 export default class AddRidesController implements IController {
-  constructor() {}
+  constructor(private rideValidator: RideValidator) {}
 
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
+    const isValid = this.rideValidator.validate(request.body)
+    if (!isValid) {
+      return {
+        statusCode: 400,
+        body: 'Missing params',
+      }
+    }
     return {
       statusCode: 201,
       body: 'Ride created successfully!',
