5f88d54bbc3c5802c3ca778c24c70af44747ea12
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-27 19:26:37 -0300

    feat: implement errorHandler on add-rides-controller

--- src/presentation/controllers/add-rides-controller.ts
+++ src/presentation/controllers/add-rides-controller.ts
@@ -1,21 +1,26 @@
 import RideValidator from '@/domain/usecases/ride-validator'
 import { IController } from '../protocols/controller-protocol'
 import { IHttpRequest, IHttpResponse } from '../protocols/http-protocol'
+import ErrorEntity from '@/domain/entities/error-entity'
+import { errorHandler } from '../helpers/error-handler'
 
 export default class AddRidesController implements IController {
   constructor(private rideValidator: RideValidator) {}
 
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const isValid = this.rideValidator.validate(request.body)
-    if (!isValid) {
-      return {
-        statusCode: 400,
-        body: 'Missing params',
+    try {
+      const isValid = this.rideValidator.validate(request.body)
+      if (!isValid) {
+        return {
+          statusCode: 400,
+          body: 'Missing params',
+        }
       }
+      return {
+        statusCode: 201,
+        body: 'Ride created successfully!',
+      }
+    } catch (error) {
+      return errorHandler(error as Error)
     }
-    return {
-      statusCode: 201,
-      body: 'Ride created successfully!',
-    }
-  }
-}
+}}
