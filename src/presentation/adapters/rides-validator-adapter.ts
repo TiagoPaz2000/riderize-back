352d70649b024dea13f762ec0de825c5829d7875
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-28 11:19:39 -0300

    feat: implement schema validator in rides-validator adapter

--- src/presentation/adapters/rides-validator-adapter.ts
+++ src/presentation/adapters/rides-validator-adapter.ts
@@ -1,7 +1,11 @@
 import RidesValidator from "@/domain/usecases/rides-validator";
+import { ridesSchema } from "./schemas/rides-validator-schema";
+
 
 export default class RidesValidatorAdapter implements RidesValidator {
-  validate(data: any): boolean {
-    return true;
+  async validate(data: any): Promise<boolean> {
+    await ridesSchema.parseAsync(data)
+
+    return true
   }
 }
