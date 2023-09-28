7d903f25879e4e7ee7a0cfe3ef1e7bbd8aa6fda2
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-28 11:18:35 -0300

    fix: change yup schema to zod schema

--- src/presentation/adapters/schemas/rides-validator-schema.ts
+++ src/presentation/adapters/schemas/rides-validator-schema.ts
@@ -1,12 +1,12 @@
 import RidesEntity from '@/domain/entities/ride-entity';
-import { object, string, number, date } from 'yup';
+import { object, string, number, date } from 'zod'
 
-export const ridesSchema = object<Omit<RidesEntity, 'id'>>({
-  name: string().required(),
-  startDate: date().required(),
-  startDateRegistration: date().required(),
-  endDateRegistration: date().required(),
-  aditionalInformation: string().nullable(),
-  startPlace: string().required(),
-  participantsLimit: number().min(0).nullable(),
+export const ridesSchema = object({
+  name: string(),
+  startDate: date(),
+  startDateRegistration: date(),
+  endDateRegistration: date(),
+  aditionalInformation: string().nullish(),
+  startPlace: string(),
+  participantsLimit: number().min(0).nullish(),
 })
