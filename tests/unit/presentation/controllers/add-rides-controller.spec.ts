354a683a96231cb02612dda2469a95c786a74757
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-28 19:36:31 -0300

    test: implement zodError if RideValidator throws

--- tests/unit/presentation/controllers/add-rides-controller.spec.ts
+++ tests/unit/presentation/controllers/add-rides-controller.spec.ts
@@ -2,6 +2,7 @@ import AddRidesController from '@/presentation/controllers/add-rides-controller'
 import { makeRideValidatorStub } from '../../stubs/usecases/ride-validator-stub'
 import { makeRidesModelStub } from '../../stubs/usecases/ride-model-stub'
 import { ridesMock } from '../../stubs/mocks/rides-mock'
+import { ZodError } from 'zod'
 
 describe('AddRidesController', () => {
   const makeSut = () => {
@@ -20,27 +21,29 @@ describe('AddRidesController', () => {
     expect(httpResponse.body).toEqual(ridesMock[0])
   })
 
-  it('should return 400 if RideValidator returns false and error message', async () => {
+  it('should return 400 and error message if RideValidator throws', async () => {
     const { sut, rideValidatorStub } = makeSut()
 
-    jest.spyOn(rideValidatorStub, 'validate').mockReturnValueOnce(Promise.resolve(false))
+    jest.spyOn(rideValidatorStub, 'validate').mockImplementationOnce(() => {
+      throw new ZodError([])
+    })
 
     const httpResponse = await sut.handle({ body: {} })
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toBe('Missing params')
+    expect(httpResponse.body).toEqual({ error: '[]' })
   })
 
-  it('should return 500 if RideValidator throws', async () => {
+  it('should return 500 if any dependencie throws an Error', async () => {
     const { sut, rideValidatorStub } = makeSut()
 
     jest.spyOn(rideValidatorStub, 'validate').mockImplementationOnce(() => {
-      throw new Error()
+      throw new Error('error')
     })
 
     const httpResponse = await sut.handle({ body: {} })
 
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toBe('Internal server error')
+    expect(httpResponse.body).toEqual({ error: 'Internal server error' })
   })
 })
