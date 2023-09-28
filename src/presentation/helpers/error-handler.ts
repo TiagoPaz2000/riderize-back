556c6793175f43139376e3a2ac383fc19d3bb5a6
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-28 19:35:26 -0300

    fix: errorHandler return object error in body

--- src/presentation/helpers/error-handler.ts
+++ src/presentation/helpers/error-handler.ts
@@ -1,3 +1,4 @@
+import { ZodError } from "zod";
 import { IHttpResponse } from "../protocols/http-protocol";
 import ErrorEntity from "@/domain/entities/error-entity";
 
@@ -5,11 +6,23 @@ export const errorHandler = (error: Error): IHttpResponse => {
   if (error instanceof ErrorEntity) {
     return {
       statusCode: error.statusCode,
-      body: error.message,
+      body: {
+        error: error.message
+      },
+    }
+  }
+  if (error instanceof ZodError) {
+    return {
+      statusCode: 400,
+      body: {
+        error: error.message
+      },
     }
   }
   return {
     statusCode: 500,
-    body: "Internal server error",
+    body: {
+      error: 'Internal server error'
+    },
   }
 }
