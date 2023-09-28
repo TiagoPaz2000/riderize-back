bb2e9c4e97b7502cb9e92a7cec5fd2b2b74019b6
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-28 19:47:47 -0300

    chore: install prisma and pg

--- src/presentation/adapters/rides-model-adapter.ts
+++ src/presentation/adapters/rides-model-adapter.ts
@@ -2,7 +2,7 @@ import RidesEntity from '@/domain/entities/ride-entity';
 import RidesModel from '@/domain/entities/rides-model';
 
 export default class RidesModelAdapter implements RidesModel {
-  add(Ride: Omit<RidesEntity, 'id'>): Promise<RidesEntity> {
+  add(ride: Omit<RidesEntity, 'id'>): Promise<RidesEntity> {
     
   }
 }
