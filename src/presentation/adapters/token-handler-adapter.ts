64bdba120c76230f7095a62fd6fbdbce0734c6c4
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-29 16:38:13 -0300

    add: implement token handler adapter

---
+++ src/presentation/adapters/token-handler-adapter.ts
@@ -0,0 +1,21 @@
+import jwt, { SignOptions } from 'jsonwebtoken'
+
+import { TokenPayload } from "@/domain/entities/token-entity";
+import TokenHandler from "@/domain/usecases/token-handler";
+
+export default class TokenHandlerAdapter implements TokenHandler{
+  private readonly secret = process.env.JWT_SECRET as string
+  private readonly options = {
+    expiresIn: '1d',
+    algorithm: 'HS256'
+  } as SignOptions
+
+  validate(token: string): TokenPayload {
+    const tokenPayload = jwt.verify(token, this.secret) as TokenPayload
+    return tokenPayload
+  }
+  generate(payload: TokenPayload): string {
+    const token = jwt.sign(payload, this.secret, this.options)
+    return token
+  }
+}
