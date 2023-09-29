d2b9c04f7e12d8fb3dba39524020ae292ef6c4b7
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-29 16:44:23 -0300

    test: move mocks to tests folder

---
+++ tests/mocks/rides-mock.ts
@@ -0,0 +1,14 @@
+import RidesEntity from "@/domain/entities/ride-entity";
+
+export const ridesMock: RidesEntity[] = [
+  {
+    id: 'any id',
+    name: 'any name',
+    startDate: new Date(),
+    startDateRegistration: new Date(),
+    endDateRegistration: new Date(),
+    startPlace: 'any place',
+    participantsLimit: null,
+    additionalInformation: 'any information',
+  }
+]
