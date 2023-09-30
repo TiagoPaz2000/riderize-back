979edaf186cee008f5818993d2e1f8c1dc94714c
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-29 22:02:52 -0300

    add: create list all rides controller

---
+++ src/presentation/controllers/list-all-rides-controller.ts
@@ -0,0 +1,23 @@
+import TokenHandler from '@/domain/usecases/token-handler';
+import { IController } from '../protocols/controller-protocol';
+import { IHttpRequest, IHttpResponse } from '../protocols/http-protocol';
+import { errorHandler } from '../helpers/error-handler';
+import RidesRepository from '@/domain/entities/rides-model';
+
+export default class ListAllRidesController implements IController {
+  constructor(private ridesRepository: RidesRepository, private tokenHandler: TokenHandler) {}
+
+  async handle(request: IHttpRequest): Promise<IHttpResponse> {
+    try {
+      this.tokenHandler.validate(request.body.authorization)
+      const rides = await this.ridesRepository.add(request.body)
+
+      return {
+        statusCode: 200,
+        body: rides,
+      }
+    } catch (error) {
+      return errorHandler(error as Error)
+    }
+  }
+} 
