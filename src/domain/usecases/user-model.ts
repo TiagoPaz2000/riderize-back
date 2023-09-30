99a26230ea5748680ffd6d482004657fb7d7614d
Tiago Paz <tiagopaz.dev@gmail.com>
2023-09-30 10:07:25 -0300

    feat: implement signin method in user repository

--- src/domain/usecases/user-model.ts
+++ src/domain/usecases/user-model.ts
@@ -2,4 +2,5 @@ import UserEntity from "../entities/user-entity";
 
 export default interface UserRepository {
   signup(user: Omit<UserEntity, 'id'>): Promise<UserEntity>
+  signin(user: Omit<UserEntity, 'id'>): Promise<UserEntity>
 }
--- src/presentation/adapters/user-repository-adapter.ts
+++ src/presentation/adapters/user-repository-adapter.ts
@@ -1,10 +1,19 @@
 import UserRepository from "@/domain/usecases/user-model";
 import connection from "../../infra/database/connection";
 import UserEntity from "@/domain/entities/user-entity";
+import ErrorEntity from "@/domain/entities/error-entity";
 
 export default class UserRepositoryAdapter implements UserRepository {
   async signup(user: Omit<UserEntity, "id">): Promise<UserEntity> {
     const newUser = await connection.user.create({ data: user })
+
+    return newUser
+  }
+
+  async signin(user: Omit<UserEntity, "id">): Promise<UserEntity> {
+    const newUser = await connection.user
+      .findUniqueOrThrow({ where: { username: user.username } })
+      .catch((_err) => { throw new ErrorEntity('User not found', 400) })
 
     return newUser
   }
