740bbb3f15ab0b6b7057f4e2cd8f6675f93cab10
Tiago Paz <tiagopaz.dev@gmail.com>
2023-10-02 10:41:53 -0300

    chore: run prisma migrate dev on command dev:compose

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response} from 'express'
 import setupApolloServer from '@/infra/server/apollo-server'
 import 'dotenv/config'
 
@@ -6,8 +6,8 @@ const app = express()
 
 setupApolloServer(app)
 
-app.get('/ping', (_req, res) => {
-  res.send('pong')
+app.get('/', (_req: Request, res: Response) => {
+  res.redirect('/graphql')
 })
 
 app.listen(3001, () => console.log('Server running on port: 3001'))
